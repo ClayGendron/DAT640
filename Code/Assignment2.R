5bc523db1f22c1c585af05bdb43a5abf8733cebb
Clay Gendron <claygendron@LAPTOP-43L123AE.localdomain>
2020-04-19 12:40:02 -0400

    updates

--- Code/Assignment2.R
+++ Code/Assignment2.R
@@ -1,5 +1,17 @@
 library(tidyverse)
 library(tidymodels)
+library(readr)
+library(rattle)
 library(here)
 
+# data pull
 
+# data("iris")
+# write.csv(as_data_frame(iris),here::here("Data","iris.csv"))
+iris <- read.csv(here::here("Data","iris.csv"))[,-1]
+
+# eda
+
+names(iris)
+dim(iris)
+summary(iris)
