52fbe554139d02358aa72a64ed1d40380e9b515e
Gendron <c.gendron1@snhu.edu>
2020-06-07 14:52:59 -0400

    final

--- Code/FinalProject.R
+++ Code/FinalProject.R
@@ -6,12 +6,12 @@ library(skimr)
 library(ggcorrplot)
 library(reshape2)
 library(grid)
+library(caret)
+library(klaR)
 
 # data pull
 
 coil <- read.delim(here::here("Data","FinalData","ticdata2000.txt"), header=FALSE)
-# write.csv(coil, file = here::here( "Data","FinalData","coil.csv"))
-
 colnames(coil) <- 
   c("MOSTYPE","MAANTHUI","MGEMOMV","MGEMLEEF","MOSHOOFD","MGODRK","MGODPR",
     "MGODOV","MGODGE","MRELGE","MRELSA","MRELOV","MFALLEEN","MFGEKIND",
@@ -26,9 +26,30 @@ colnames(coil) <-
     "AMOTSCO","AVRAAUT","AAANHANG","ATRACTOR","AWERKT","ABROM","ALEVEN",
     "APERSONG","AGEZONG","AWAOREG","ABRAND","AZEILPL","APLEZIER","AFIETS",
     "AINBOED","ABYSTAND","CARAVAN")
+# write.csv(coil, file = here::here( "Data","FinalData","coil.csv"))
+
+coil2 <- read.delim(here::here("Data","FinalData","ticeval2000.txt"), header=FALSE)
+colnames(coil2) <- 
+  c("MOSTYPE","MAANTHUI","MGEMOMV","MGEMLEEF","MOSHOOFD","MGODRK","MGODPR",
+    "MGODOV","MGODGE","MRELGE","MRELSA","MRELOV","MFALLEEN","MFGEKIND",
+    "MFWEKIND","MOPLHOOG","MOPLMIDD","MOPLLAAG","MBERHOOG","MBERZELF","MBERBOER",
+    "MBERMIDD","MBERARBG","MBERARBO","MSKA","MSKB1","MSKB2","MSKC",
+    "MSKD","MHHUUR","MHKOOP","MAUT1","MAUT2","MAUT0","MZFONDS",
+    "MZPART","MINKM30","MINK3045","MINK4575","MINK7512","MINK123M",
+    "MINKGEM","MKOOPKLA","PWAPART","PWABEDR","PWALAND","PPERSAUT","PBESAUT",
+    "PMOTSCO","PVRAAUT","PAANHANG","PTRACTOR","PWERKT","PBROM","PLEVEN",
+    "PPERSONG","PGEZONG","PWAOREG","PBRAND","PZEILPL","PPLEZIER","PFIETS",
+    "PINBOED","PBYSTAND","AWAPART","AWABEDR","AWALAND","APERSAUT","ABESAUT",
+    "AMOTSCO","AVRAAUT","AAANHANG","ATRACTOR","AWERKT","ABROM","ALEVEN",
+    "APERSONG","AGEZONG","AWAOREG","ABRAND","AZEILPL","APLEZIER","AFIETS",
+    "AINBOED","ABYSTAND","CARAVAN")
+#write.csv(coil2, file = here::here( "Data","FinalData","coiltest.csv"))
 
 summary(coil)
 skim(coil)
+
+train <- coil
+train <-  coil2
 
 # correlation plots
 
@@ -54,3 +75,7 @@ plot2 <- ggplot(plot_df, aes(x = MINKGEM)) +
   geom_line(aes(y = POLICIES))
 
 grid.draw(rbind(ggplotGrob(plot1), ggplotGrob(plot2)))
+
+# build model naive bayes
+
+
