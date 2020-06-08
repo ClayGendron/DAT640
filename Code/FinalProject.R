679f0a0337e469028762904ec381f5b684c75e97
Gendron <c.gendron1@snhu.edu>
2020-06-07 23:14:38 -0400

    final commit

--- Code/FinalProject.R
+++ Code/FinalProject.R
@@ -46,13 +46,15 @@ colnames(coil2) <-
     "AINBOED","ABYSTAND")
 # write.csv(coil2, file = here::here( "Data","FinalData","coiltest.csv"))
 
+coil2caravan <- read.delim(here::here("Data","FinalData","tictgts2000.txt"), header=FALSE)
+colnames(coil2caravan) <- c("CARAVAN")
 # summarize data and understand distributions
 
 summary(coil)
 skim(coil)
 
 train <- coil
-test <-  coil2
+test <- cbind(coil2,coil2caravan)
 
 # correlation plots
 
@@ -114,7 +116,7 @@ colnames(mod_test) <-
     "PINBOED","PBYSTAND","AWAPART","AWABEDR","AWALAND","APERSAUT","ABESAUT",
     "AMOTSCO","AAANHANG","ATRACTOR","ABROM","ALEVEN",
     "APERSONG","AGEZONG","AWAOREG","ABRAND","AZEILPL","APLEZIER","AFIETS",
-    "AINBOED","ABYSTAND")
+    "AINBOED","ABYSTAND","CARAVAN")
 
 mod_train$CARAVAN <- factor(mod_train$CARAVAN)
 summary(mod_train)
@@ -126,12 +128,15 @@ caravan_rec <- recipe(CARAVAN ~., data = mod_train) %>%
     step_BoxCox(all_numeric())
 train_rec <- prep(caravan_rec, training = mod_train)
 nb_train <- bake(train_rec, new_data = mod_train)
+nb_test <- bake(train_rec, new_data = mod_test)
 
 # train model
 
 nb_caravan_mod <- NaiveBayes(CARAVAN ~., data = nb_train)
 train_p <- nb_caravan_mod %>% predict(nb_train)
+test_p <- nb_caravan_mod %>% predict(nb_test)
 mod_train_p <- cbind(nb_train,train_p)
+mod_test_p <- cbind(nb_test,test_p)
 
 # model assesment
 
@@ -145,9 +150,25 @@ ROCRperf <- performance(ROCRpred, "tpr", "fpr")
 
 plot(ROCRperf)
 
-riskchart(train_pred,
-          nb_train$CARAVAN, 
-          title="Model Performance: Cross-Selling Caravan Insurance to Customers", 
-          recall.name="Targeted Cross-Selling", precision.name = "Strick Rate",
-          show.lift=TRUE, show.precision=TRUE, legend.horiz=FALSE, show.maximal = TRUE) %>% print()
+train_riskchart <- riskchart(train_pred,
+                      nb_train$CARAVAN, 
+                      title="Model Performance: Cross-Selling Caravan Insurance to Customers", 
+                      recall.name="Targeted Cross-Selling", precision.name = "Strick Rate",
+                      show.lift=TRUE, show.precision=TRUE, legend.horiz=FALSE, show.maximal = TRUE)
+print(train_riskchart)
+
+# assessing against the test data
+
+cf_matrix_test <- table(mod_test_p$CARAVAN,mod_test_p$class)
+caret::confusionMatrix(cf_matrix_test)
+
+test_pred <- as.numeric(test_p$posterior[,c(2)])
+test_lab <- as.numeric(nb_test$CARAVAN)
+
+test_riskchart <- riskchart(test_pred,
+                    nb_test$CARAVAN, 
+                    title="(New Data) Model Performance: Cross-Selling Caravan Insurance to Customers", 
+                    recall.name="Targeted Cross-Selling", precision.name = "Strick Rate",
+                    show.lift=TRUE, show.precision=TRUE, legend.horiz=FALSE, show.maximal = TRUE)
+print(test_riskchart)
 
